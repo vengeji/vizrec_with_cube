8ee351ecd4cfa240134d4d4e83d29b217905970b
VenLoji <vengeji@stu.scu.edu.cn>
2019-05-05 12:42:41 +0800

    -

--- ml/timer.py
+++
@@ -1,9 +0,0 @@
-from contextlib import contextmanager
-import time
-
-
-@contextmanager
-def venloji_timer(name):
-    start = time.time()
-    yield
-    print('[{0}] done in \t\t {1} s'.format(name, time.time() - start))
